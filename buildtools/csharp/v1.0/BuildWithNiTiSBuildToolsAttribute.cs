28b7ca88dd64b630cb95354ff8bd340854a80a52
NickName73 <egorooz73@gmail.com>
2022-06-12 17:02:50 +0300

    Some fixes

--- buildtools/csharp/v1.0/BuildWithNiTiSBuildToolsAttribute.cs
+++ buildtools/csharp/v1.0/BuildWithNiTiSBuildToolsAttribute.cs
@@ -1,8 +1,10 @@
 // Disable by <NBT_SignAssembly>false</NBT_SignAssembly>
+[assembly:NiTiS.Build.Tools.BuildWithNiTiSBuildTools]
+
+
 namespace NiTiS.Build.Tools {
 	[System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
 	[System.ComponentModel.Browsable(false)]
 	[System.AttributeUsage(System.AttributeTargets.Assembly, Inherited=true)]
 	internal class BuildWithNiTiSBuildToolsAttribute : System.Attribute {}
 }
-[assembly:BuildWithNiTiSBuildToolsAttribute]
