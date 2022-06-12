79ae2e9998cdb6608d863a2b9e53c28d1cc112cd
NickName73 <egorooz73@gmail.com>
2022-06-12 17:28:18 +0300

    Some fixes

--- buildtools/csharp/v1.0/BuildWithNiTiSBuildToolsAttribute.cs
+++ buildtools/csharp/v1.0/BuildWithNiTiSBuildToolsAttribute.cs
@@ -6,5 +6,5 @@ namespace NiTiS.Build.Tools {
 	[System.ComponentModel.EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
 	[System.ComponentModel.Browsable(false)]
 	[System.AttributeUsage(System.AttributeTargets.Assembly, Inherited=true)]
-	internal class BuildWithNiTiSBuildToolsAttribute : System.Attribute {}
+	internal sealed class BuildWithNiTiSBuildToolsAttribute : System.Attribute {}
 }
